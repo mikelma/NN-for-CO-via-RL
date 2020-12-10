ccf7edcb94992d8f0e52efbf6c1695d74dcfe38c
mikelma <mikelma7@gmail.com>
2020-12-10 12:27:25 +0100

    borda added and cleanup

--- nn_vanila_pg.py
+++ nn_vanila_pg.py
@@ -11,17 +11,16 @@ import models
 NOISE_LEN = 128
 N_SAMPLES = 64
 LR = .0003
-ITERS = 2000
-LOSS_FUNC = 'L2'
+ITERS = 4000
+LOSS_FUNC = 'L3'
 C = 40
 EVAL_INVERSE = True
+BORDA = False
 
 DEVICE = 'cpu'
 LOG_FILE = None
-# N = 20
-# INSTANCE = '../../instances/PFSP/tai20_5_8.fsp'
-N = 50
-INSTANCE = '../../instances/PFSP/tai50_5_8.fsp'
+N = 20
+INSTANCE = '../../instances/PFSP/tai20_5_8.fsp'
 
 
 dl = DataLogger({'instance': INSTANCE.split('/')[-1],
@@ -49,6 +48,11 @@ for it in range(ITERS):
     samples = models.sample(distribution, N_SAMPLES)
 
     permus = [utils.marina2permu(v) for v in samples.cpu()]
+    if BORDA:
+        borda = np.array(pypermu.utils.borda(permus))
+        inv_borda = utils.permu2inverse(borda)
+        permus = [np.array(pypermu.utils.compose(p, inv_borda))
+                  for p in permus]
 
     if EVAL_INVERSE:
         # transform the permus list into a list of it's inverse permutations
@@ -80,12 +84,6 @@ for it in range(ITERS):
     with torch.no_grad():
         logp = models.log_probs_unprocessed(samples, distribution)
         h_list = utils.entropy(distribution, reduction='none')
-        # dl.push(
-        #     other={
-        #         'logp v0': logp[0], 'logp v10': logp[10], 'logp v17': logp[17],
-        #         'H v0': h_list[0], 'H v10': h_list[10], 'H v17': h_list[17],
-        #         'Pmax v0': distribution[0].probs.max(), 'Pmax v17': distribution[17].probs.max(),
-        #     })
         dl.push(other={'iteration': it,
                        'entropy': h.item(),
                        'loss': loss.item(),
@@ -95,28 +93,5 @@ for it in range(ITERS):
 
 if LOG_FILE != None:
     dl.to_csv(LOG_FILE, ITERS)
-
-# ax1 = plt.subplot(2, 1, 1)
-# ax1.plot(range(ITERS), dl.log['logp v0'], label='logp v0', alpha=.75)
-# ax1.plot(range(ITERS), dl.log['logp v10'], label='logp v10', alpha=.75)
-# ax1.plot(range(ITERS), dl.log['logp v17'], label='logp v17', alpha=.75)
-# ax1.set_xlabel('Iterations')
-# ax1.set_ylabel('Log probability')
-# ax1.legend(loc=1)
-#
-# ax2 = ax1.twinx()
-# ax2.set_ylabel('Entropy')
-# ax2.plot(range(ITERS), dl.log['H v0'], '--', label='H v0', alpha=.75)
-# ax2.plot(range(ITERS), dl.log['H v10'], '--', label='H v10', alpha=.75)
-# ax2.plot(range(ITERS), dl.log['H v17'], '--', label='H v17', alpha=.75)
-# ax2.legend(loc=4)
-#
-# plt.subplot(2, 1, 2)
-# plt.plot(range(ITERS), dl.log['Pmax v0'], label='Pmax v0')
-# plt.plot(range(ITERS), dl.log['Pmax v17'], label='Pmax v17')
-# plt.legend()
-#
-# plt.title("Dissection of the loss function (loss function: "+LOSS_FUNC+")")
-# plt.show()
 
 dl.plot()
