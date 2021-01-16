5f5c6c2c5f748a2ad5b4b4e77999e84ed59987f3
mikelma <mikelma7@gmail.com>
2021-01-16 12:31:19 +0100

    new pypermu lib function and wandb integration

--- nn_vanila_pg.py
+++ nn_vanila_pg.py
@@ -2,10 +2,10 @@ import pypermu
 import torch
 from torch.optim import Adam
 import numpy as np
-from datalog import DataLogger
 import utils
 import loss_funcs
 import models
+import os
 import uuid
 import argparse
 
@@ -15,75 +15,59 @@ parser.add_argument('--instance-size', metavar='N', type=int, nargs=1,
                     required=True, help='Size of the instance', choices=[20, 50])
 parser.add_argument('--instance', metavar='FILE', type=str, nargs=1,
                     required=True, help='Path to the instance file')
-parser.add_argument('--log', metavar='FILE', type=str, nargs='?', default=False, const=True,
+parser.add_argument('--log', type=str, nargs='?', default=False, const=True,
                     required=False, help='If this falg is set, the logger will be stored as a CSV')
+parser.add_argument('--wandb', type=str, nargs='?', default=False, const=True,
+                    required=False, help='If this falg is provided, weight and biases will be used to track the experiment')
 args = parser.parse_args()
 
-N = args.instance_size[0]
-INSTANCE = args.instance[0]
 WRITE_LOG = False if args.log == False else True
-
-##############################
-#   Common hyperparameters   #
-##############################
+WANDB_ENABLE = False if args.wandb == False else True
 DEVICE = 'cpu'
+instance_path = args.instance[0]
 
-LOSS_FUNC = 'L5'
-C = 40
-GAMMA = .02
-EVAL_INVERSE = True
-BORDA = False
-##############################
-##############################
+config = {'instance': instance_path.split('/')[-1],
+          'instance size': args.instance_size[0],
+          'max iters': 1000,
+          'n samples': 64,
+          'learning rate': .0003,
+          'noise length': 128,
+          'C': 40,
+          'loss function': 'L6',
+          'eval inverse': True,
+          'borda': False,
+          'model': models.SimpleModel,
+          'gamma': .02,
+          }
 
-#----------------------------#
-#       params: N=20         #
-#----------------------------#
-if N == 20:
-    NOISE_LEN = 128
-    N_SAMPLES = 64
-    LR = .0003
-    ITERS = 2000
-    MODEL = models.SimpleModel
-#----------------------------#
+if WRITE_LOG:
+    from datalog import DataLogger
+    dl = DataLogger(config)
 
-#----------------------------#
-#       params: N=50         #
-#----------------------------#
-if N == 50:
-    NOISE_LEN = 128
-    N_SAMPLES = 64
-    LR = .0003
-    ITERS = 3000
-    MODEL = models.SimpleModel
-#----------------------------#
+if WANDB_ENABLE:
+    import wandb
+    wandb.init('NN-PG-loss-test', config=config)
 
-dl = DataLogger({'instance': INSTANCE.split('/')[-1],
-                 'instance size': N,
-                 'max iters': ITERS,
-                 'num. samples': N_SAMPLES,
-                 'learning rate': LR,
-                 'noise length': NOISE_LEN,
-                 'C': C,
-                 'loss function': LOSS_FUNC,
-                 'eval inverse': EVAL_INVERSE,
-                 'model': MODEL.name,
-                 'gamma': GAMMA,
-                 })
+# initializations
+problem = pypermu.problems.pfsp.Pfsp(instance_path)
 
-problem = pypermu.problems.pfsp.Pfsp(INSTANCE)
-model = MODEL(NOISE_LEN, N, device=DEVICE)
+model = config['model'](config['noise length'],
+                        config['instance size'], device=DEVICE)
 model.to(DEVICE)
-optimizer = Adam(model.parameters(), lr=LR)
+optimizer = Adam(model.parameters(), lr=config['learning rate'])
 
-for it in range(ITERS):
-    print('DEVICE: ', DEVICE)
+if WANDB_ENABLE:
+    wandb.watch(model, loss_funcs.compute_l6, log='all', log_freq=10)
+
+for it in range(config['max iters']):
     # forward pass
-    noise = torch.rand(NOISE_LEN).to(DEVICE)
+    noise = torch.rand(config['noise length']).to(DEVICE)
     distribution = model.get_distribution(noise)
-    samples = models.sample(distribution, N_SAMPLES)
+    samples = models.sample(distribution, config['n samples'])
 
-    permus = [utils.marina2permu(v) for v in samples.cpu()]
+    # convert the sampled marina vectors to their permutation representations
+    permus = pypermu.utils.transformations.marina2permu_batched(
+        samples.cpu().numpy())
 
     ###########################
     # print('distrib array recording')
@@ -94,13 +78,13 @@ for it in range(ITERS):
     # np.save('arrays/{}'.format(it), m)
     ###########################
 
-    if BORDA:
+    if config['borda']:
         borda = np.array(pypermu.utils.borda(permus))
         inv_borda = utils.permu2inverse(borda)
         permus = [np.array(pypermu.utils.compose(p, inv_borda))
                   for p in permus]
 
-    if EVAL_INVERSE:
+    if config['eval inverse']:
         # transform the permus list into a list of it's inverse permutations
         permus = [utils.permu2inverse(permu) for permu in permus]
 
@@ -108,46 +92,64 @@ for it in range(ITERS):
     fitness_list = torch.tensor(
         problem.evaluate(permus)).float().to(DEVICE)
 
-    dl.push(fitness_list=fitness_list.cpu().numpy())
+    #################################################
+    if WANDB_ENABLE:
+        wandb.log({
+            'min fitness': fitness_list.min().item(),
+            'mean fitness': fitness_list.mean().item(),
+        })
+
+    if WRITE_LOG:
+        dl.push(fitness_list=fitness_list.cpu().numpy())
+    #################################################
 
     optimizer.zero_grad()  # clear gradient buffers
 
-    if LOSS_FUNC == 'L2':
+    if config['loss function'] == 'L2':
         loss = loss_funcs.compute_l2(
-            samples, distribution, fitness_list, C)
+            samples, distribution, fitness_list, config['C'])
 
-    elif LOSS_FUNC == 'L3':
+    elif config['loss function'] == 'L3':
         loss = loss_funcs.compute_l3(samples, distribution,
                                      fitness_list, N=len(distribution))
-    elif LOSS_FUNC == 'L4':
+    elif config['loss function'] == 'L4':
         loss = loss_funcs.compute_l4(
             samples, distribution, fitness_list)
-    elif LOSS_FUNC == 'L5':
+
+    elif config['loss function'] == 'L5':
         loss, convergency, scaled_logps = loss_funcs.compute_l5(
-            samples, distribution, fitness_list, gamma=GAMMA)
+            samples, distribution, fitness_list, gamma=config['gamma'])
+
+    elif config['loss function'] == 'L6':
+        loss, convergency = loss_funcs.compute_l6(samples, distribution,
+                                                  fitness_list)
 
     loss.backward()  # update gradient buffers
     optimizer.step()  # update model's parameters
 
     # --------------------- logger --------------------- #
-    with torch.no_grad():
-        h = utils.entropy(distribution, reduction='none')
-        if WRITE_LOG:
-            other = {}
+    if WRITE_LOG or WANDB_ENABLE:
+        with torch.no_grad():
             print('Recording entropies')
-            for i in range(N):
-                other['h'+str(i)] = h[i].item()
-            dl.push(other=other)
-        h = h.sum()
-        dl.push(other={'iteration': it,
-                       'entropy': h.item(),
-                       'loss': loss.item(),
-                       'convergency': convergency.item(),
-                       'scaled logps': scaled_logps.item()
-                       })
-        if not WRITE_LOG:
-            print(it+1, '/', ITERS, end=' ')
-            dl.print()
+            h = utils.entropy(distribution, reduction='none')
+            entropies = {}
+            for i in range(config['instance size']):
+                entropies['h'+str(i)] = h[i].item()
+
+            data = {'iteration': it, 'entropy': h.sum().item(
+            ), 'loss': loss.item(), 'convergency': convergency.item()}
+            merged = {**entropies, **data}
+
+            if WRITE_LOG:
+                dl.push(other=merged)
+            if WANDB_ENABLE:
+                wandb.log(merged)
+    else:
+        print(it+1, '/', config['max iters'])
+
+if WANDB_ENABLE:
+    torch.onnx.export(model, noise, "model.onnx")
+    wandb.save("model.onnx")
 
 if WRITE_LOG:
-    dl.to_csv(str(uuid.uuid4())+'.csv', ITERS)
+    dl.to_csv('runs/'+str(uuid.uuid4())+'.csv', config['max iters'])
